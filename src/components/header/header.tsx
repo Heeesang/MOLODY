e6ce311753de736ce270c26edbb2ca4aa43280be
Heesang <ghs050611@naver.com>
2025-03-01 03:48:44 +0900

    add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,21 @@
 export default function Header() {
   return (
-    <nav className="w-full bg-black text-white p-4">
-      하이
+    <nav className="fixed w-full text-white h-16">
+      <div className="max-w-[1140px] h-full m-auto flex">
+        <div className="flex h-full w-full items-center justify-between">
+            <div>
+              <img src="./molody_logo.svg" className=""/>
+            </div>
+            <div className="flex items-center text-[#4E5968]">
+              <div className="px-3">
+                <a>무료 구독</a>
+              </div>
+              <div className="px-3">
+                <a>음악 추천</a>
+              </div>
+            </div>
+        </div>
+      </div>
     </nav>
   );
-}
+} 

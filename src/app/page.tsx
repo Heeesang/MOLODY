ce9879ef53e92da66f17f0c1d7070b6be4abc0f4
Heesang <ghs050611@naver.com>
2025-03-01 21:50:22 +0900

    add mainContent text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import MainContent from "@/components/contents/mainContent";
 import Header from "@/components/header/header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Header/> 
+      <Header/>
+      <MainContent/>
     </>
   );
 }
---
+++ src/components/contents/mainContent.tsx
@@ -0,0 +1,10 @@
+export default function MainContent() {
+    return (
+      <div className="h-dvh flex justify-center">
+        <div className="flex flex-col items-center pt-48">
+          <h1 className="text-6xl font-bold">morning melody를 메일로 보내드립니다!</h1>
+          <h2 className="text-3xl font-medium mt-3 text-stone-400">음악으로 기분 좋은 하루를 시작하세요</h2>
+        </div>
+      </div>
+    );
+  } 

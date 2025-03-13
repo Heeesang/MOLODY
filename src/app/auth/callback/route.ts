83a40ac1e259241e5fc908af694f9fbd20c6d2d3
Heesang <ghs050611@naver.com>
2025-03-13 22:44:50 +0900

    update console

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,21 +6,18 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get('code');
-  const next = searchParams.get('next') ?? '/#';
 
   console.log('Full callback URL:', request.url);
-  console.log('Code from query:', code);
 
   if (code) {
     const supabase = createClient();
     const { data, error } = await (await supabase).auth.exchangeCodeForSession(code);
 
     console.log('Session data:', data);
-    console.log('Exchange error:', error);
 
     if (!error) {
-      console.log('Redirecting to:', `${origin}${next}`);
-      return NextResponse.redirect(`${origin}${next}`);
+      console.log('Redirecting to:', `${origin}`);
+      return NextResponse.redirect(`${origin}`);
     } else {
       console.error('Code exchange failed:', error.message);
     }

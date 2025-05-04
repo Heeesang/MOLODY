09d951e3f862bb51dad176bfbf3a8ba3aeaf0380
Heesang <ghs050611@naver.com>
2025-05-04 17:53:21 +0900

    remove header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import MainContent from "@/components/contents/main/mainContent";
-import Header from "@/components/header/header";
 
 export default function Home() {
   return (
     <>
-      <Header/>
       <MainContent/>
     </>
   );

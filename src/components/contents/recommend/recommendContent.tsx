e8d4a5e9cc005cbd6a820a902dcfe62f599274a9
Heesang <ghs050611@naver.com>
2025-05-13 23:46:32 +0900

    Add component

--- src/components/contents/recommend/recommendContent.tsx
+++ src/components/contents/recommend/recommendContent.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
 export default function RecommendContent() {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-full">
-            <h1 className="text-2xl font-bold">추천 음악</h1>
-            <p className="mt-4 text-gray-500">아직 추천 음악이 없습니다.</p>
+        <div className="flex flex-col items-center justify-center w-full h-dvh">
+            <div className="flex max-w-[1140px] w-full h-full justify-between items-center">
+                <div className="h-9/12 flex flex-col">
+                    <h1 className="text-2xl font-bold mb-3">최근 추천 음악</h1>
+                </div>
+                <div className="border border-gray-200 rounded-lg w-96 h-9/12 flex flex-col justify-end items-center">
+                    <Image
+                        src="/molody_logo.svg"
+                        alt="Molody Logo"
+                        width={100}
+                        height={40}
+                        className="mb-14"
+                    />
+                </div>
+            </div>
         </div>
     );
 }

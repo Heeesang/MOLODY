573f633135ee081e25a0c96210ceb2ffbbacc2d5
Heesang <ghs050611@naver.com>
2025-06-06 23:53:08 +0900

    Update button UI

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
     return (
       <button
         onClick={onClick}
-        className="bg-black text-white text-lg font-bold px-6 py-3 rounded-lg hover:bg-gray-700 transition-colors"
+        className="bg-black text-white text-lg font-bold px-6 py-3 rounded-lg hover:bg-neutral-700 transition-colors"
       >
         {text}
       </button>
